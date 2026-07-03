8d3ce3df3e30c9d85d3b495d92ffab44fd43b7f9
acesavvy101 <gracielladeronne07@gmail.com>
2026-07-03 23:50:02 +0800

    feat: add initial homepage layout

---
+++ src/home.js
@@ -0,0 +1,9 @@
+//import the image to use
+import cafeBg from "/assets/cafeBg.jpg"
+
+export const homepageImg = document.createElement('img')
+homepageImg.src = cafeBg
+
+export const description = document.createElement('p')
+description.textContent = "We are a lovely humble cafe hidden in the old streets of Tamsui. Run by a local grandpa, serving affordable drinks with a quiet relaxing scenery."
+
